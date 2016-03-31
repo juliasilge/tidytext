f7b9ba3160a7f3effcc174b56a8a742195db0aed
dgrtwo <dgrtwo@princeton.edu>
2016-03-31 17:23:59 -0400

    Finished partsofspeech saving

--- R/setup_data.R
+++ R/setup_data.R
@@ -10,8 +10,31 @@ setup_data <- function() {
 
   devtools::use_data(stopwords, overwrite = TRUE)
 
+  parts_of_speech <- readr::read_csv("Noun,N
+Plural,p
+Noun Phrase,h
+Verb (usu participle),V
+Verb (transitive),t
+Verb (intransitive),i
+Adjective,A
+Adverb,v
+Conjunction,C
+Preposition,P
+Interjection,!
+Pronoun,r
+Definite Article,D
+Indefinite Article,I
+Nominative,o
+", col_names = c("pos", "code"))
+
   # parts of speech
   moby_file <- system.file("extdata", "mobyposi.i", package = "tidytext")
-  mobypos <- readr::read_delim(moby_file, delim = "\xd7", ) %>%
-    tidyr::unnest(pos = str_split(AN, ""))
+  partsofspeech <- readr::read_delim(moby_file, delim = "\xd7",
+                               col_names = c("word", "code")) %>%
+    tidyr::unnest(code = str_split(code, "")) %>%
+    inner_join(parts_of_speech, by = "code") %>%
+    mutate(word = str_to_lower(word)) %>%
+    distinct()
+
+  devtools::use_data(partsofspeech, overwrite = TRUE)
 }
