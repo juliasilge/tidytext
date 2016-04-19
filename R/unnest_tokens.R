0bfbf90de1c21a2ee4706adf218ab7c4f7954cd5
juliasilge <julia.silge@gmail.com>
2016-04-19 16:09:59 -0600

    Change to "token =" and use input/output for params. Resolves #1.

--- R/unnest_tokens.R
+++ R/unnest_tokens.R
@@ -1,21 +1,21 @@
 #' Split a column into tokens using either the tokenizer package or str_split
 #'
 #' @param tbl Data frame
-#' @param token_col Token column to be created
-#' @param text_col Text column that gets split
-#' @param method Method for tokenizing. If tokenizer package
+#' @param output_col Output column to be created
+#' @param input_col Input column that gets split
+#' @param token Unit for tokenizing. If tokenizer package
 #' is installed, options are "characters", "words", "sentences", "lines",
 #' "paragraphs", and "regex". Otherwise, using str_split the option is "words".
 #' Default is "words".
 #' @param to_lower Whether to turn column lowercase
-#' @param drop Whether original text column should get dropped. Ignored
-#' if the original text and new token column have the same name.
-#' @param token Token column to be created as bare name
-#' @param text Text column that gets split as bare name
+#' @param drop Whether original input column should get dropped. Ignored
+#' if the original input and new output column have the same name.
+#' @param output Output column to be created as bare name
+#' @param input Input column that gets split as bare name
 #' @param ... Extra arguments passed on to the tokenizer
 #'
-#' @details If the method for tokenizing is sentences, lines, paragraphs, or
-#' regex, the entire text will be collapsed together before tokenizing.
+#' @details If the unit for tokenizing is sentences, lines, paragraphs, or
+#' regex, the entire input will be collapsed together before tokenizing.
 #'
 #' @import dplyr
 #' @import tokenizers
@@ -35,38 +35,38 @@
 #'   unnest_tokens(word, txt)
 #'
 #' d %>%
-#'   unnest_tokens(word, txt, method = "sentences")
+#'   unnest_tokens(word, txt, token = "sentences")
 #'
 #' @export
-unnest_tokens_ <- function(tbl, token_col, text_col, method = "words",
+unnest_tokens_ <- function(tbl, output_col, input_col, token = "words",
                           to_lower = TRUE, drop = TRUE, ...) {
-  if (method %in% c("sentences", "lines", "paragraphs", "regex")) {
+  if (token %in% c("sentences", "lines", "paragraphs", "regex")) {
     exps <- list(substitute(stringr::str_c(colname, collapse = "\n"),
-                            list(colname = as.name(text_col))))
-    names(exps) <- text_col
-    tbl <- group_by_(tbl, .dots = setdiff(colnames(tbl), text_col)) %>%
+                            list(colname = as.name(input_col))))
+    names(exps) <- input_col
+    tbl <- group_by_(tbl, .dots = setdiff(colnames(tbl), input_col)) %>%
       summarise_(.dots = exps)
   }
-  col <- tbl[[text_col]]
+  col <- tbl[[input_col]]
   if (to_lower) {
     col <- stringr::str_to_lower(col)
   }
 
-  method <- paste0("tokenize_", method)
-  tokenfunc <- get(method)
-  if (method == "tokenize_characters" || method == "tokenize_words") {
-    tbl[[token_col]] <- tokenfunc(col, lowercase = FALSE, ...)
+  token <- paste0("tokenize_", token)
+  tokenfunc <- get(token)
+  if (token == "tokenize_characters" || token == "tokenize_words") {
+    tbl[[output_col]] <- tokenfunc(col, lowercase = FALSE, ...)
   } else { # mash the whole character string together here for other tokenizer functions
-    tbl[[token_col]] <- tokenfunc(col, ...)
+    tbl[[output_col]] <- tokenfunc(col, ...)
   }
 
-  if (drop && text_col != token_col) {
-    tbl[[text_col]] <- NULL
+  if (drop && input_col != output_col) {
+    tbl[[input_col]] <- NULL
   }
 
-  ret <- tidyr::unnest_(tbl, token_col)
+  ret <- tidyr::unnest_(tbl, output_col)
 
-  ret <- ret[ret[[token_col]] != "", ]
+  ret <- ret[ret[[output_col]] != "", ]
 
   ret
 }
@@ -74,11 +74,11 @@ unnest_tokens_ <- function(tbl, token_col, text_col, method = "words",
 
 #' @rdname unnest_tokens
 #' @export
-unnest_tokens <- function(tbl, token, text, method = "words",
+unnest_tokens <- function(tbl, output, input, token = "words",
                            to_lower = TRUE, drop = TRUE, ...) {
-  token_col <- col_name(substitute(token))
-  text_col <- col_name(substitute(text))
+  output_col <- col_name(substitute(output))
+  input_col <- col_name(substitute(input))
 
-  unnest_tokens_(tbl, token_col, text_col, method = method,
+  unnest_tokens_(tbl, output_col, input_col, token = token,
                  to_lower = to_lower, drop = drop, ...)
 }
