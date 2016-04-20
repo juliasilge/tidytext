79c3fe72e45ed082d5832b3d90b32cebb29374b8
Dave Robinson <drobinson@stackoverflow.com>
2016-04-20 16:51:37 -0400

    Added functions for casting to a sparse matrix, such as a DocumentTermMatrix or TermDocumentMatrix. Fixed a few issues in the existing tidiers, including deleting the general Matrix tidiers that were moved to broom
    Moved relevant description of such operations into its own vignette.

---
+++ R/sparse_casters.R
@@ -0,0 +1,144 @@
+#' Standard-evaluation version of cast_sparse
+#'
+#' @param data A tbl
+#' @param row_col String version of column to use as row names
+#' @param column_col String version of column to use as column names
+#' @param value_col String version of column to use as sparse matrix
+#' values, or a numeric vector to use. Default 1 (to create a binary
+#' matrix)
+#' @param ... Extra arguments to pass on to \code{\link{sparseMatrix}}
+#'
+#' @import Matrix
+#' @export
+cast_sparse_ <- function(data, row_col, column_col, value_col = 1,
+                         ...) {
+  row_names <- data[[row_col]]
+  col_names <- data[[column_col]]
+  if (is.numeric(value_col)) {
+    values <- value_col
+  } else {
+    values <- data[[value_col]]
+  }
+
+  # if it's a factor, preserve ordering
+  if (is.factor(row_names)) {
+    row_u <- levels(row_names)
+    i <- as.integer(row_names)
+  } else {
+    row_u <- unique(row_names)
+    i <- match(row_names, row_u)
+  }
+
+  if (is.factor(col_names)) {
+    col_u <- levels(col_names)
+    j <- as.integer(col_names)
+  } else {
+    col_u <- unique(col_names)
+    j <- match(col_names, col_u)
+  }
+
+  ret <- Matrix::sparseMatrix(i = i, j = j, x = values,
+                              dimnames = list(row_u, col_u), ...)
+
+  ret
+}
+
+
+#' Create a sparse matrix from row names, column names, and values
+#' in a table.
+#'
+#' @param data A tbl
+#' @param row A bare column name to use as row names in sparse matrix
+#' @param column A bare column name to use as column names in sparse matrix
+#' @param value A bare column name to use as sparse matrix values, default 1
+#'
+#' @return A sparse Matrix object, with one row for each unique value in
+#' the \code{row} column, one column for each unique value in the \code{column}
+#' column, and with as many non-zero values as there are rows in \code{data}.
+#'
+#' @examples
+#'
+#' dat <- data.frame(a = c("row1", "row1", "row2", "row2", "row2"),
+#'                   b = c("col1", "col2", "col1", "col3", "col4"),
+#'                   val = 1:5)
+#'
+#' cast_sparse(dat, a, b)
+#'
+#' cast_sparse(dat, a, b, val)
+#'
+#' @export
+cast_sparse <- function(data, row, column, value) {
+  if (missing(value)) {
+    value_col <- 1
+  } else {
+    value_col <- col_name(substitute(value))
+    if (is.null(data[[value_col]])) {
+      value_col <- value
+    }
+  }
+
+  cast_sparse_(data,
+               col_name(substitute(row)),
+               col_name(substitute(column)),
+               value_col)
+}
+
+
+#' Casting a data frame to
+#' a DocumentTermMatrix or a TermDocumentMatrix
+#'
+#' This turns a "tidy" one-term-per-dopument-per-row data frame into a
+#' DocumentTermMatrix or TermDocumentMatrix from the tm package. Each
+#' can be called either with non-standard evaluation (bare column names)
+#' or character vectors (for \code{cast_tdm_} and \code{cast_dtm_}).
+#'
+#' @param data Table with one-term-per-document-per-row
+#' @param term,term_col (Bare) name of a column with terms
+#' @param document,document_col (Bare) name of a column with documents
+#' @param value,value_col (Bare) name of a column containing values
+#' @param weighting The weighting function for the DTM/TDM
+#' (default is term-frequency, effectively unweighted)
+#' @param ... Extra arguments passed on to
+#' \code{\link{sparseMatrix}}
+#'
+#' @rdname cast_tdm
+#' @export
+cast_tdm_ <- function(data, term_col, document_col, value_col = 1,
+                      weighting = tm::weightTf, ...) {
+  m <- cast_sparse_(data, term_col, document_col, value_col, ...)
+  tm::as.TermDocumentMatrix(m, weighting = weighting)
+}
+
+
+#' @rdname cast_tdm
+#' @export
+cast_tdm <- function(data, term, document, value = 1,
+                      weighting = tm::weightTf, ...) {
+  cast_tdm_(data,
+            col_name(substitute(term)),
+            col_name(substitute(document)),
+            col_name(substitute(value)),
+            weighting = weighting, ...)
+}
+
+
+
+#' @rdname cast_tdm
+#' @export
+cast_dtm_ <- function(data, document_col, term_col, value_col = 1,
+                      weighting = tm::weightTf, ...) {
+  m <- cast_sparse_(data, document_col, term_col, value_col, ...)
+  tm::as.DocumentTermMatrix(m, weighting = weighting)
+}
+
+
+#' @rdname cast_tdm
+#' @export
+cast_dtm <- function(data, document, term, value = 1,
+                     weighting = tm::weightTf, ...) {
+  cast_dtm_(data,
+            col_name(substitute(document)),
+            col_name(substitute(term)),
+            col_name(substitute(value)),
+            weighting = weighting, ...)
+}
--- R/sparse_tidiers.R
+++ R/sparse_tidiers.R
@@ -1,59 +1,54 @@
-#' Tidy a sparseMatrix object from the Matrix package
+#' Tidy DocumentTermMatrix, TermDocumentMatrix, and related objects
+#' from the tm package
 #'
-#' Tidy a sparseMatrix object from the Matrix package into
-#' a three-column data frame, row, column, and value (with
-#' zeros missing). If there are row names or column names,
-#' use those, otherwise use indices
+#' Tidy a DocumentTermMatrix or TermDocumentMatrix into
+#' a three-column data frame: \code{term{}}, and value (with
+#' zeros missing), with one-row-per-term-per-document.
 #'
 #' @importFrom broom tidy
 #'
-#' @param x A Matrix object
+#' @param x A DocumentTermMatrix or TermDocumentMatrix object
+#' @param row_names Specify row names
+#' @param col_names Specify column names
 #' @param ... Extra arguments, not used
 #'
-#' @name sparse_tidiers
+#' @name tdm_tidiers
+#'
+#' @examples
+#'
+#' if (requireNamespace("topicmodels", quietly = TRUE)) {
+#'   data("AssociatedPress", package = "topicmodels")
+#'   AssociatedPress
+#'
+#'   tidy(AssociatedPress)
+#' }
 #'
 #' @export
-tidy.dgTMatrix <- function(x, ...) {
-  tidy_triplet(x, Matrix::summary(x))
-}
-
-
-#' @rdname sparse_tidiers
-#' @export
-tidy.dgCMatrix <- function(x, ...) {
-  tidy(as(x, "dgTMatrix"))
-}
-
-
-#' @rdname sparse_tidiers
-#' @export
-tidy.sparseMatrix <- function(x, ...) {
-  tidy(as(x, "dgTMatrix"))
-}
-
-
-tidy.simple_triplet_matrix <- function(x, ...) {
-  triplets <- unclass(x)[c("i", "j", "v")]
-  names(triplets) <- c("i", "j", "x")
-  tidy_triplet(x, triplets)
-}
-
-
-#' @rdname sparse_tidiers
-#' @export
 tidy.DocumentTermMatrix <- function(x, ...) {
-  ret <- tidy.simple_triplet_matrix(x)
+  ret <- tidy.simple_triplet_matrix(x, x$dimnames$Docs, x$dimnames$Terms)
   colnames(ret) <- c("document", "term", "count")
+
   ret
 }
 
 
-#' @rdname sparse_tidiers
+#' @rdname tdm_tidiers
 #' @export
 tidy.TermDocumentMatrix <- function(x, ...) {
-  ret <- tidy.simple_triplet_matrix(x)
+  ret <- tidy.simple_triplet_matrix(x, x$dimnames$Terms, x$dimnames$Docs)
   colnames(ret) <- c("term", "document", "count")
   ret
+}
+
+
+#' @rdname tdm_tidiers
+#' @export
+tidy.simple_triplet_matrix <- function(x,
+                                       row_names = NULL,
+                                       col_names = NULL, ...) {
+  triplets <- unclass(x)[c("i", "j", "v")]
+  names(triplets) <- c("i", "j", "x")
+  tidy_triplet(x, triplets, row_names, col_names)
 }
 
 
@@ -61,13 +56,19 @@ tidy.TermDocumentMatrix <- function(x, ...) {
 #'
 #' @param x Object with rownames and colnames
 #' @param triplets A data frame or list of i, j, x
-tidy_triplet <- function(x, triplets) {
+#' @param row_names rownames, if not gotten from rownames(x)
+#' @param col_names colnames, if not gotten from colnames(x)
+tidy_triplet <- function(x, triplets, row_names = NULL, col_names = NULL) {
   row <- triplets$i
-  if (!is.null(rownames(x))) {
+  if (!is.null(row_names)) {
+    row <- row_names[row]
+  } else if (!is.null(rownames(x))) {
     row <- rownames(x)[row]
   }
   col <- triplets$j
-  if (!is.null(colnames(x))) {
+  if (!is.null(col_names)) {
+    col <- col_names[col]
+  } else if (!is.null(colnames(x))) {
     col <- colnames(x)[col]
   }
 
@@ -77,5 +78,5 @@ tidy_triplet <- function(x, triplets) {
 
 
 #' @export
-tidy
+broom::tidy
 
