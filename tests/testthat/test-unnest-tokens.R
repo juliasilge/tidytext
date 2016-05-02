66c532b246dc1892a133648e85101f78c3faff80
juliasilge <julia.silge@gmail.com>
2016-05-02 14:44:36 -0600

    Fix tests for ngram/skip_ngram

--- tests/testthat/test-unnest-tokens.R
+++ tests/testthat/test-unnest-tokens.R
@@ -32,7 +32,7 @@ test_that("tokenizing by sentence works", {
   expect_equal(d$sentence[1], "i'm nobody!")
 })
 
-test_that("tokenizing by ngram works", {
+test_that("tokenizing by ngram and skip ngram works", {
   d2 <- data_frame(txt = c("Hope is the thing with feathers -",
                            "That perches in the soul -",
                            "And sings the tune without the words -",
@@ -45,19 +45,20 @@ test_that("tokenizing by ngram works", {
                            "And on the strangest Sea -",
                            "Yet - never - in Extremity,",
                            "It asked a crumb - of me."))
+
+  # tokenize by ngram
   d <- d2 %>% unnest_tokens(ngram, txt, token = "ngrams", n = 2)
   expect_equal(nrow(d), 57)
   expect_equal(ncol(d), 1)
   expect_equal(d$ngram[1], "hope is")
   expect_equal(d$ngram[10], "and sings")
-})
 
-test_that("tokenizing by skip ngram works", {
+  # tokenize by skip_ngram
   d <- d2 %>% unnest_tokens(ngram, txt, token = "skip_ngrams", n = 4, k = 2)
   expect_equal(nrow(d), 36)
   expect_equal(ncol(d), 1)
   expect_equal(d$ngram[1], "hope is the thing")
   expect_equal(d$ngram[10], "tune without the words")
-})
 
+})
 
