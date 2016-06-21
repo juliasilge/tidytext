c0b27686049cb158c06da385df584b4f386851f6
dgrtwo <dgrtwo@princeton.edu>
2016-06-20 23:20:52 -0400

    New TF-IDF version, using tapply and table. See #11

--- R/calculate_tf_idf.R
+++ R/calculate_tf_idf.R
@@ -16,9 +16,8 @@
 #' If the dataset is grouped, the groups are ignored but are
 #' retained.
 #'
-#' Note that the dataset does not necessarily need exactly one row per
-#' document-term combination; the per-document term frequency will sum
-#' \code{n_col} across rows if necessary.
+#' The dataset must have exactly one row per document-term combination
+#' for this to work.
 #'
 #' @examples
 #'
@@ -39,17 +38,6 @@
 #'
 #' @export
 calculate_tf_idf <- function(tbl, term_col, document_col, n_col) {
-
-  ############# beginning of matrix math for new version tf-idf
-  dtm <- cast_sparse_(tbl,
-                     col_name(substitute(document_col)),
-                     col_name(substitute(term_col)),
-                     col_name(substitute(n_col)))
-  tf <- dtm/Matrix::rowSums(dtm)
-  idf <- log(nrow(dtm)/Matrix::colSums(abs(sign(dtm))))
-  tfidf <- tf*idf
-  ############# end of new version draft
-
   calculate_tf_idf_(tbl,
                     col_name(substitute(term_col)),
                     col_name(substitute(document_col)),
@@ -60,45 +48,15 @@ calculate_tf_idf <- function(tbl, term_col, document_col, n_col) {
 #' @rdname calculate_tf_idf
 #' @export
 calculate_tf_idf_ <- function(tbl, term_col, document_col, n_col) {
-  g <- groups(tbl)
+  terms <- tbl[[term_col]]
+  documents <- tbl[[document_col]]
+  n <- tbl[[n_col]]
+  doc_totals <- tapply(n, documents, sum)
+  idf <- log(length(doc_totals) / table(terms))
 
-  total_documents <- n_distinct(tbl[[document_col]])
+  tbl$tf <- tbl[[n_col]] / as.numeric(doc_totals[documents])
+  tbl$idf <- as.numeric(idf[terms])
+  tbl$tf_idf <- tbl$tf * tbl$idf
 
-  by_doc_dots <- list(.document_total = substitute(sum(x), list(x = as.name(n_col))))
-  # by_term_dots <- list(.nterm = substitute(sum(x), list(x = as.name(n_col))),
-  #                      .ndocs = substitute(n_distinct(x), list(x = as.name(document_col))))
-  by_term_dots <- list(.nterm = substitute(x, list(x = as.name(n_col))),
-                       .ndocs = substitute(n_distinct(x), list(x = as.name(document_col))))
-  ungrouped_dots <- list(tf = substitute(.nterm / .document_total),
-                         idf = substitute(log(total_documents / .ndocs)))
-
-
-  # Julia's new version -- does this work right?
-
-  total_terms <- tbl %>% group_by_(document_col) %>%
-    summarize_(.dots = by_doc_dots)
-  ret <- left_join(tbl, total_terms) %>%
-    mutate_(.dots = by_term_dots) %>%
-    ungroup() %>%
-    mutate_(.dots = ungrouped_dots) %>%
-    mutate(tf_idf = tf * idf) %>%
-    select(-.ndocs, -.nterm, -.document_total)
-
-
-  # Dave's old version -- is this wrong? or right?
-  # ret <- tbl %>%
-  #   group_by_(document_col) %>%
-  #   mutate_(.dots = by_doc_dots) %>%
-  #   group_by_(term_col) %>%
-  #   mutate_(.dots = by_term_dots) %>%
-  #   ungroup() %>%
-  #   mutate_(.dots = ungrouped_dots) %>%
-  #   mutate(tf_idf = tf * idf) %>%
-  #   select(-.ndocs, -.nterm, -.document_total)
-
-  if (!is.null(g)) {
-    ret <- group_by_(ret, .dots = g)
-  }
-
-  ret
+  tbl
 }
