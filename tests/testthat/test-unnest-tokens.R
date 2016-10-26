juliasilge/tidytext

commit c5215b1c4b2766ebd600482fb73f87fedb6f45a2

Changed unnest_tokens so that it no longer uses tidyr's unnest, but rather a custom version that removes some overhead. In some experiments, this sped up unnest_tokens on large inputs by about 40%. This also moves tidyr from Imports to Suggests for now.
unnest_tokens now checks that there are no list columns in the input, and raises an error if present (since those cannot be unnested)

Added appropriate unit tests for the above

R/unnest_tokens.R
@@ -59,6 +59,10 @@
 #' @export
 unnest_tokens_ <- function(tbl, output_col, input_col, token = "words",
                           to_lower = TRUE, drop = TRUE, collapse = NULL, ...) {
+  if (any(!purrr::map_lgl(tbl, is.atomic))) {
+    stop("unnest_tokens expects all columns of input to be atomic vectors (not lists)")
+  }
+
   if (is.function(token)) {
     tokenfunc <- token
   } else {
@@ -85,13 +89,19 @@ unnest_tokens_ <- function(tbl, output_col, input_col, token = "words",
   }
 
   col <- tbl[[input_col]]
-  tbl[[output_col]] <- tokenfunc(col, ...)
+  output_lst <- tokenfunc(col, ...)
 
-  if (drop && input_col != output_col) {
+  if (!(is.list(output_lst) && length(output_lst) == nrow(tbl))) {
+    stop("Expected output of tokenizing function to be a list of length ",
+         nrow(tbl))
+  }
+
+  if (drop) {
     tbl[[input_col]] <- NULL
   }
 
-  ret <- tidyr::unnest_(tbl, output_col)
+  ret <- tbl[rep(seq_len(nrow(tbl)), lengths(output_lst)), ]
+  ret[[output_col]] <- unlist(output_lst)
 
   if (to_lower) {
     ret[[output_col]] <- stringr::str_to_lower(ret[[output_col]])

tests/testthat/test-unnest-tokens.R
@@ -86,3 +86,17 @@ test_that("tokenizing with a custom function works", {
   expect_equal(d2$unit[2], "nobody")
   expect_equal(d2$unit[4], "you know!")
 })
+
+test_that("unnest_tokens raises an error if there is a list column present", {
+  d <- data_frame(a = c("hello world", "goodbye world"), b = list(1:2, 3:4))
+  expect_error(unnest_tokens(d, word, a), "atomic vectors")
+})
+
+test_that("unnest_tokens raises an error if custom tokenizer gives bad output", {
+  d <- data_frame(txt = "Emily Dickinson")
+
+  expect_error(unnest_tokens(d, word, txt, token = function(e) c("a", "b")),
+               "to be a list")
+  expect_error(unnest_tokens(d, word, txt, token = function(e) list("a", "b")),
+               "of length")
+})

DESCRIPTION
@@ -16,7 +16,6 @@ RoxygenNote: 5.0.1
 Depends: R (>= 2.10)
 Imports: dplyr,
     stringr,
-    tidyr,
     plyr,
     broom,
     Matrix,
@@ -25,6 +24,7 @@ Imports: dplyr,
     purrr (>= 0.1.1),
     methods
 Suggests: readr,
+    tidyr,
     XML,
     tm,
     wordcloud,

commit afc24f4cee22ae7cbbf7b697f6cbfada81d5e4e3

Added drop = FALSE to subsetting operations in unnest_tokens, fixing bug I introduced in c5215b1c4b2766ebd600482fb73f87fedb6f45a2
Added unit tests that catch this bug

R/unnest_tokens.R
@@ -121,14 +121,14 @@ unnest_tokens_ <- function(tbl, output_col, input_col, token = "words",
     tbl[[input_col]] <- NULL
   }
 
-  ret <- tbl[rep(seq_len(nrow(tbl)), lengths(output_lst)), ]
+  ret <- tbl[rep(seq_len(nrow(tbl)), lengths(output_lst)), , drop = FALSE]
   ret[[output_col]] <- unlist(output_lst)
 
   if (to_lower) {
     ret[[output_col]] <- stringr::str_to_lower(ret[[output_col]])
   }
 
-  ret <- ret[ret[[output_col]] != "", ]
+  ret <- ret[ret[[output_col]] != "", , drop = FALSE]
 
   ret
 }

tests/testthat/test-unnest-tokens.R
@@ -132,9 +132,35 @@ test_that("tokenizing LaTeX works", {
   expect_equal(res2$row, c(1, 1, 3))
 })
 
+test_that("Tokenizing a one-column data.frame works", {
+  text <- data.frame(txt = c("Because I could not stop for Death -",
+                             "He kindly stopped for me -"),
+                     stringsAsFactors = FALSE)
+  d <- unnest_tokens(text, word, txt)
+
+  expect_is(d, "data.frame")
+  expect_equal(nrow(d), 12)
+  expect_equal(ncol(d), 1)
+  expect_equal(d$word[1], "because")
+})
+
+test_that("Tokenizing a two-column data.frame with one non-text column works", {
+  text <- data.frame(line = 1:2,
+                  txt = c("Because I could not stop for Death -",
+                          "He kindly stopped for me -"),
+                  stringsAsFactors = FALSE)
+  d <- unnest_tokens(text, word, txt)
+
+  expect_is(d, "data.frame")
+  expect_equal(nrow(d), 12)
+  expect_equal(ncol(d), 2)
+  expect_equal(d$word[1], "because")
+  expect_equal(d$line[1], 1)
+})
 
 test_that("Trying to tokenize a non-text format with words raises an error", {
   d <- data_frame(txt = "Emily Dickinson")
   expect_error(unnest_tokens(d, word, txt, token = "sentences", format = "latex"),
                "except words")
 })
+