f360297bc982013248571a97905c53afed6aa535
juliasilge <julia.silge@gmail.com>
2017-05-01 20:27:23 -0600

    Changes to input for quanteda::dfm

--- tests/testthat/test-sparse-tidiers.R
+++ tests/testthat/test-sparse-tidiers.R
@@ -26,7 +26,8 @@ test_that("Can tidy DocumentTermMatrices and TermDocumentMatrices", {
 test_that("Can tidy dfm from quanteda", {
   if (requireNamespace("quanteda", quietly = TRUE)) {
     library(methods)
-    capture.output(dfm_obj <- quanteda::dfm(quanteda::data_corpus_inaugural))
+    capture.output(dfm_obj <- quanteda::dfm(quanteda::data_corpus_inaugural,
+                                            verbose = FALSE))
 
     dfm_obj_td <- tidy(dfm_obj)
 
