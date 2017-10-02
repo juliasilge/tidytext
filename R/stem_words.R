226d8702b503d88d14f4f375aa4425290ea9bda8
ColinFay <contact@colinfay.me>
2017-10-02 22:04:44 +0200

    stemming function

---
+++ R/stem_words.R
@@ -0,0 +1,30 @@
+#' Word stemming
+#'
+#' @param df the data.frame containing the unnested text
+#' @param col the column containing the words. Default is word.
+#'
+#' @importFrom proustr pr_stem_words
+#'
+#' @return a tibble with stemmed words
+#' @export
+#'
+#' @examples
+#' library(janeaustenr)
+#' library(dplyr)
+#' original_books <- austen_books() %>%
+#'  group_by(book) %>%
+#'  mutate(linenumber = row_number()) %>%
+#'  ungroup()
+#'
+#' library(tidytext)
+#' tidy_books <- original_books %>%
+#'  unnest_tokens(word, text) %>%
+#'  stem_words()
+#'
+#'  tidy_books
+#'
+
+stem_words <- function(df, col = word, language = "english"){
+  col <- rlang::enquo(col)
+  proustr::pr_stem_words(df, !!col, language = language)
+}
