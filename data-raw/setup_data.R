40d5f7be797364ba085adbb396d5d4080f6d35c9
Julia Silge <julia.silge@gmail.com>
2017-12-27 20:48:25 -0700

    Add dataset of negators, modals, and adverbs that can affect sentiment analysis. Closes #55.

--- data-raw/setup_data.R
+++ data-raw/setup_data.R
@@ -96,3 +96,21 @@ parts_of_speech <- readr::read_delim("data-raw/mobyposi.i.zip", delim = "\xd7",
 
 readr::write_csv(parts_of_speech, "data-raw/parts_of_speech.csv")
 devtools::use_data(parts_of_speech, overwrite = TRUE)
+
+
+# stop_words dataset ------------------------------------------------------
+
+nma_words <- readr::read_lines("data-raw/list-English-negators.txt") %>%
+  tibble(word = .) %>%
+  mutate(modifier = "negator") %>%
+  bind_rows(readr::read_lines("data-raw/list-English-modals.txt") %>%
+              tibble(word = .) %>%
+              mutate(modifier = "modal")) %>%
+  bind_rows(readr::read_lines("data-raw/list-English-adverbs.txt") %>%
+              tibble(word = .) %>%
+              mutate(modifier = "adverb"))
+
+
+readr::write_csv(nma_words, "data-raw/nma_words.csv")
+devtools::use_data(nma_words, overwrite = TRUE)
+
