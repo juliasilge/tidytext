6c94acc2b4c520e0fe0a19473f825e3800ccc3bd
colin <colin@thinkr.fr>
2019-08-02 22:57:22 +0200

    Function and test for unnest_ngrams

---
+++ R/unnest_ngrams.R
@@ -0,0 +1,52 @@
+#' Wrapper around unnest_tokens & ngram
+#'
+#' This function is a wrapper around `unnest_tokens( token = "ngrams" )`.
+#'
+#' @seealso
+#' + [unnest_tokens()]
+#'
+#' @inheritParams unnest_tokens
+#' @inheritParams tokenizers::tokenize_ngrams
+#'
+#' @export
+#' @importFrom dplyr enquo
+#'
+#' @examples
+#' library(dplyr)
+#' library(janeaustenr)
+#'
+#' d <- tibble(txt = prideprejudice)
+#' d
+#' d %>%
+#'   unnest_ngrams(word, txt)
+#'
+unnest_ngrams <- function(
+  tbl,
+  output,
+  input,
+  n = 3L,
+  n_min = n,
+  stopwords = character(),
+  ngram_delim = " ",
+  simplify = FALSE,
+  format = c("text", "man", "latex", "html", "xml"),
+  to_lower = TRUE,
+  drop = TRUE,
+  collapse = NULL
+){
+  format <- match.arg(format)
+  unnest_tokens(tbl,
+                !! enquo(output),
+                !! enquo(input),
+                format = format,
+                to_lower = to_lower,
+                drop = to_lower,
+                collapse = to_lower,
+                token = "ngrams",
+                n = n,
+                n_min = n_min,
+                stopwords = stopwords,
+                ngram_delim = ngram_delim,
+                simplify = simplify
+  )
+}
---
+++ tests/testthat/helper-funs.R
@@ -0,0 +1,21 @@
+expect_nrow <- function(tbl, n){
+  expect_is(tbl, "data.frame")
+  expect_equal(nrow(tbl), n)
+}
+
+expect_first_row <- function(tbl, col, text){
+  ct <- tbl %>%
+    pull(!!enquo(col)) %>%
+    purrr::pluck(1)
+  expect_match(ct, text)
+}
+
+skspr <- data.frame(
+  id = 1:4,
+  txt = c(
+    "Now is the winter of our discontent",
+    "Made glorious summer by this sun of York;",
+    "And all the clouds that lour'd upon our house",
+    "In the deep bosom of the ocean buried."
+  )
+)
