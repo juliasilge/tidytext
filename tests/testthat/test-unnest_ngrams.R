5c2a19f7ec484236a00f45eab82a488d64dd8bd9
Colin Fay <contact@colinfay.me>
2019-09-21 22:22:38 +0200

    rm song from test

--- tests/testthat/test-unnest_ngrams.R
+++ tests/testthat/test-unnest_ngrams.R
@@ -11,15 +11,6 @@ test_that("unnest_ngrams works", {
 })
 
 test_that("unnest_skip_ngrams works", {
-  song <-  paste0("How many roads must a man walk down\n",
-                  "Before you call him a man?\n",
-                  "How many seas must a white dove sail\n",
-                  "Before she sleeps in the sand?\n",
-                  "\n",
-                  "How many times must the cannonballs fly\n",
-                  "Before they're forever banned?\n",
-                  "The answer, my friend, is blowin' in the wind.\n",
-                  "The answer is blowin' in the wind.\n")
   r <- unnest_skip_ngrams(song_df, out, txt, n = 4)
   s <- unnest_tokens(song_df, out, txt, n = 4, token = "skip_ngrams")
   expect_equal(r, s)
