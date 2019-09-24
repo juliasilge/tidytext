2fee48a630796358c0d2e27b5b5d66303246682c
Julia Silge <julia.silge@gmail.com>
2019-09-23 21:58:29 -0600

    Changes to unnest for char/char shingles. Addresses #153.

--- R/unnest_char.R
+++ R/unnest_char.R
@@ -1,4 +1,4 @@
-#' Wrapper around unnest_tokens & characters / character_shingles
+#' Wrapper around unnest_tokens for characters and character shingles
 #'
 #' Thes functions are a wrapper around `unnest_tokens( token = "characters" )`
 #' and `unnest_tokens( token = "character_shingles" )`.
@@ -6,9 +6,9 @@
 #' @seealso
 #' + [unnest_tokens()]
 #'
-#' @inheritParams unnest_tokens
 #' @inheritParams tokenizers::tokenize_characters
 #' @inheritParams tokenizers::tokenize_character_shingles
+#' @inheritParams unnest_tokens
 #'
 #' @export
 #' @importFrom dplyr enquo
@@ -19,15 +19,18 @@
 #' library(janeaustenr)
 #'
 #' d <- tibble(txt = prideprejudice)
-#' d
+#'
 #' d %>%
 #'   unnest_characters(word, txt)
+#'
+#' d %>%
+#'   unnest_character_shingles(word, txt, n = 3)
+#'
 unnest_characters <- function(
   tbl,
   output,
   input,
   strip_non_alphanum = TRUE,
-  simplify = FALSE,
   format = c("text", "man", "latex", "html", "xml"),
   to_lower = TRUE,
   drop = TRUE,
@@ -38,13 +41,12 @@ unnest_characters <- function(
   unnest_tokens(tbl,
                 !! enquo(output),
                 !! enquo(input),
+                token = "characters",
                 format = format,
                 to_lower = to_lower,
                 drop = to_lower,
                 collapse = to_lower,
-                token = "characters",
                 strip_non_alphanum = strip_non_alphanum,
-                simplify = simplify,
                 ...
   )
 }
@@ -60,7 +62,6 @@ unnest_character_shingles <- function(
   n = 3L,
   n_min = n,
   strip_non_alphanum = TRUE,
-  simplify = FALSE,
   format = c("text", "man", "latex", "html", "xml"),
   to_lower = TRUE,
   drop = TRUE,
@@ -71,15 +72,14 @@ unnest_character_shingles <- function(
   unnest_tokens(tbl,
                 !! enquo(output),
                 !! enquo(input),
+                token = "character_shingles",
                 format = format,
                 to_lower = to_lower,
                 drop = to_lower,
                 collapse = to_lower,
-                token = "character_shingles",
                 n = n,
                 n_min = n_min,
                 strip_non_alphanum = strip_non_alphanum,
-                simplify = simplify,
                 ...
   )
 }
