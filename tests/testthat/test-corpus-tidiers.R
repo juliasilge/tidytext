d8ad155842a5d568c44402f717b50d97b44ec4fd
Julia Silge <julia.silge@gmail.com>
2020-03-03 14:29:49 -0700

    Switch to skip_if_not_installed

--- tests/testthat/test-corpus-tidiers.R
+++ tests/testthat/test-corpus-tidiers.R
@@ -1,68 +1,64 @@
 context("Corpus tidiers")
 
 test_that("Can tidy corpus from tm package", {
-  if (require("tm", quietly = TRUE)) {
-    #' # tm package examples
-    txt <- system.file("texts", "txt", package = "tm")
-    ovid <- VCorpus(DirSource(txt, encoding = "UTF-8"),
-                    readerControl = list(language = "lat")
-    )
+  skip_if_not_installed("tm")
+  # tm package examples
+  txt <- system.file("texts", "txt", package = "tm")
+  ovid <- VCorpus(DirSource(txt, encoding = "UTF-8"),
+                  readerControl = list(language = "lat")
+  )
 
-    td <- tidy(ovid, collapse = " ")
+  td <- tidy(ovid, collapse = " ")
 
-    expect_equal(length(ovid), nrow(td))
-    expect_equal(paste(as.character(ovid[[1]]), collapse = " "), td$text[1])
-  }
+  expect_equal(length(ovid), nrow(td))
+  expect_equal(paste(as.character(ovid[[1]]), collapse = " "), td$text[1])
+
 })
 
 
 test_that("Can tidy corpus from quanteda package", {
-  if (requireNamespace("quanteda", quietly = TRUE)) {
-    #' # tm package examples
-    data("data_corpus_inaugural", package = "quanteda")
+  skip_if_not_installed("quanteda")
+  data("data_corpus_inaugural", package = "quanteda")
 
-    texts <- quanteda::texts(data_corpus_inaugural)
+  texts <- quanteda::texts(data_corpus_inaugural)
 
-    td <- tidy(data_corpus_inaugural)
+  td <- tidy(data_corpus_inaugural)
 
-    expect_equal(length(texts), nrow(td))
-    expect_true(all(td$text == texts))
-  }
+  expect_equal(length(texts), nrow(td))
+  expect_true(all(td$text == texts))
 })
 
 test_that("Can tidy corpus from quanteda using accessor functions", {
-  if (requireNamespace("quanteda", quietly = TRUE)) {
-    x <- quanteda::data_corpus_inaugural
+  skip_if_not_installed("quanteda")
+  x <- quanteda::data_corpus_inaugural
 
-    ## old method
-    ret_old <- tbl_df(data.frame(texts = quanteda::texts(x),
-                                 quanteda::docvars(x),
-                                 stringsAsFactors = FALSE)) %>%
-      rename(text = texts)
+  ## old method
+  ret_old <- tibble(data.frame(texts = quanteda::texts(x),
+                               quanteda::docvars(x),
+                               stringsAsFactors = FALSE)) %>%
+    rename(text = texts)
 
-    ## new method
-    ret_new <- tidy(x)
+  ## new method
+  ret_new <- tidy(x)
 
-    expect_identical(ret_old, ret_new)
-  }
+  expect_identical(ret_old, ret_new)
 })
 
 test_that("Can glance a corpus from quanteda using accessor functions", {
-  if (requireNamespace("quanteda", quietly = TRUE)) {
-    x <- quanteda::data_corpus_inaugural
+  skip_if_not_installed("quanteda")
+  x <- quanteda::data_corpus_inaugural
 
-    ## old method
-    glance_old <- function(x, ...) {
-      md <- purrr::compact(quanteda::metacorpus(x))
-      # turn vectors into list columns
-      md <- purrr::map_if(md, ~ length(.) > 1, list)
-      as_tibble(md)
-    }
-    ret_old <- glance_old(x)
-
-    ## new method
-    ret_new <- glance(x)
-
-    expect_identical(ret_old, ret_new)
+  ## old method
+  glance_old <- function(x, ...) {
+    md <- purrr::compact(quanteda::metacorpus(x))
+    # turn vectors into list columns
+    md <- purrr::map_if(md, ~ length(.) > 1, list)
+    as_tibble(md)
   }
+  ret_old <- glance_old(x)
+
+  ## new method
+  ret_new <- glance(x)
+
+  expect_identical(ret_old, ret_new)
 })
