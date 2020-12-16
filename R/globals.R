f446648fe30c60c7c019897dc445668316fe3af6
Julia Silge <julia.silge@gmail.com>
2020-12-15 22:32:58 -0700

    Add new_groups to globals

--- R/globals.R
+++ R/globals.R
@@ -1,6 +1,6 @@
 globalVariables(c(
   "X1", "X2", "X3", "word", "code", "category", "texts",
   "i", "j", "x", "Var1", "Var2", "value",
-  ".ndocs", ".nterm", ".document_total",
+  ".ndocs", ".nterm", ".document_total", "new_groups",
   "tf", "idf", "sentiments", "document", "term"
 ))
