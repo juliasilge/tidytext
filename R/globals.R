51e55d7e76c31b7d90ec4d93f90156c202386cf8
Julia Silge <julia.silge@gmail.com>
2022-05-09 10:08:51 -0600

    Add missing global var

--- R/globals.R
+++ R/globals.R
@@ -2,5 +2,5 @@ globalVariables(c(
   "X1", "X2", "X3", "word", "code", "category", "texts",
   "i", "j", "x", "Var1", "Var2", "value", "out",
   ".ndocs", ".nterm", ".document_total", "new_groups",
-  "tf", "idf", "sentiments", "document", "term"
+  "tf", "idf", "sentiments", "document", "term", "L1"
 ))
