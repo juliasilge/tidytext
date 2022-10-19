d5a7ed7c18f6971c313a9bb53b3c4f9888589faa
Julia Silge <julia.silge@gmail.com>
2022-10-19 14:30:23 -0600

    Add another global variable

--- R/globals.R
+++ R/globals.R
@@ -1,6 +1,6 @@
 globalVariables(c(
   "X1", "X2", "X3", "word", "code", "category", "texts",
-  "i", "j", "x", "Var1", "Var2", "value", "out",
+  "i", "j", "x", "Var1", "Var2", "value", "out", "topic",
   ".ndocs", ".nterm", ".document_total", "new_groups",
   "tf", "idf", "sentiments", "document", "term", "L1"
 ))
